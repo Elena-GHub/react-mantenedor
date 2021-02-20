78a630e3e142d83af79a338df9a929418d59ee7e
Elena-GHub <cabjelen@yahoo.es>
2021-02-20 14:47:05 +0100

    added  to Edit button selected user path to Form view

--- src/App.js
+++ src/App.js
@@ -10,18 +10,27 @@ class App extends Component {
     data: [],
     path: 'list',
   }
-
+  
   constructor() {
     super()
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then(({ data }) => this.setState({ data }))
   }
 
+  selectUser = id => {
+    this.setState({
+      path: 'form',
+      selectedUser: id,
+    })
+  }
   render() {
     const { path, data } = this.state
     return (
       <div className="App">
-        {path === 'list' && <ViewList data={data}/>}
+        {path === 'list' && <ViewList 
+          handleClick={this.selectUser}
+          data={data}
+        />}
         {path === 'form' && <UserForm />}
       </div>
     );
