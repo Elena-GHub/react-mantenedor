eb2a3757d4eecab5901b3b49be9019534ee7c16f
Elena-GHub <cabjelen@yahoo.es>
2021-02-21 14:27:20 +0100

    added validate method to form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 
+const validate = values => {
+    const errors = {}
+    if (!values.name) {
+        errors.name = "This field must be filled in"
+    }
+    if (!values.email) {
+        errors.email = "This field must be filled in"
+    }
+    if (!values.website) {
+        errors.website = "This field must be filled in"
+    }
+    
+    return errors
+}
+
 export default class UserForm extends Component {
-    state = {}
+    state = {
+        errors: {}
+    }
 
     handleChange = ({ target }) => {
         this.setState({
             [target.name]: target.value,
         })
     }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        const { errors, ...noErrors } = this.state
+        const result = validate(noErrors)
+        this.setState({ errors: result })
+        if (!Object.keys(result).length) {
+            // send form
+            e.target.reset()
+        }
+    }
+
     render() {
-        console.log(this.state)
+        const { errors } = this.state
         return (
-            <form>
-                <input name="name" onChange={this.handleChange} />
-                <input name="email" onChange={this.handleChange} />
-                <input name="website" onChange={this.handleChange} />
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="Name" name="name" onChange={this.handleChange} />
+                {errors.name && <p>{errors.name}</p>}
+                <input placeholder="Email" name="email" onChange={this.handleChange} />
+                {errors.email && <p>{errors.email}</p>}
+                <input placeholder="Website" name="website" onChange={this.handleChange} />
+                {errors.website && <p>{errors.website}</p>}
+                <input type="submit" value="Send" />
             </form>
         )
     }
